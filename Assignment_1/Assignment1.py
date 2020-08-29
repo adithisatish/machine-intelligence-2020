dffa030d19b8508da6584242069acaad75c48923
Adithi Satish <45226748+adithisatish@users.noreply.github.com>
2020-08-29 11:24:18 +0530

    added definitions for all functions

--- Assignment_1/Assignment1.py
+++ Assignment_1/Assignment1.py
@@ -17,8 +17,10 @@ def get_entropy_of_dataset(df):
 
 	for i in values:
 		p = len(df[df[target]==i]) #the number of instances in the dataset where target = i
-		print(p)
-		res = -(p/size)*np.log2([p/size])[0]
+		if p==0:
+			res = 0
+		else:
+			res = -(p/size)*np.log2([p/size])[0]
 		entropy+=res
 	
 	#print(entropy)
@@ -31,6 +33,16 @@ def get_entropy_of_dataset(df):
 	#output:int/float/double/large
 def get_entropy_of_attribute(df,attribute):
 	entropy_of_attribute = 0
+	size = len(df) #the total number of instances in the dataset
+	target = list(df.columns)[-1] #the target variable i.e Nth column in the dataset
+	values = list(df[attribute].unique()) #all possible values the given attribute can take
+
+	for i in values:
+		new_df = df[df[attribute]==i]
+		entropy = get_entropy_of_dataset(new_df)
+		entropy_of_attribute+= len(new_df)/size * entropy
+
+	#print(entropy_of_attribute)
 	return abs(entropy_of_attribute)
 
 
@@ -40,6 +52,11 @@ def get_entropy_of_attribute(df,attribute):
 	#output:int/float/double/large
 def get_information_gain(df,attribute):
 	information_gain = 0
+
+	entropy_df = get_entropy_of_dataset(df)
+	avg_info = get_entropy_of_attribute(df,attribute)
+	
+	information_gain = entropy_df - avg_info
 	return information_gain
 
 
@@ -51,13 +68,21 @@ def get_selected_attribute(df):
    
 	information_gains={}
 	selected_column=''
-
+	
 	'''
 	Return a tuple with the first element as a dictionary which has IG of all columns 
 	and the second element as a string with the name of the column selected
 
 	example : ({'A':0.123,'B':0.768,'C':1.23} , 'C')
 	'''
+
+	columns = list(df.columns)[:-1] #All columns except the target variable
+
+	for i in columns:
+		information_gains[i] = get_information_gain(df,i)
+	
+	max_info_gain = max(information_gains.values())
+	selected_column = [i for i in information_gains.keys() if information_gains[i]==max_info_gain][0]
 
 	return (information_gains,selected_column)
 
