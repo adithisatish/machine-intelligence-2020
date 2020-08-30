57597edef62a1069621d60b460ddb97b4b076ac3
AnanyaV0308 <57029203+AnanyaV0308@users.noreply.github.com>
2020-08-30 21:59:20 +0530

    Update Assignment1.py

--- Assignment_1/Assignment1.py
+++ Assignment_1/Assignment1.py
@@ -31,15 +31,15 @@ def get_entropy_of_dataset(df):
 
 	entropy = 0
 	size = len(df) #the total number of instances in the dataset
+	if size==0:
+		return entropy
+	
 	target = list(df.columns)[-1] #the target variable i.e Nth column in the dataset
 	values = list(df[target].unique()) #all possible values the target variable can take
 
 	for i in values:
 		p = len(df[df[target]==i]) #the number of instances in the dataset where target = i
-		if p==0:
-			res = 0
-		else:
-			res = -(p/size)*np.log2([p/size])[0]
+		res = -(p/size)*np.log2([p/size])[0]
 		entropy+=res
 	
 	#print(entropy)
